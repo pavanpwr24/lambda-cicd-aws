427af6e27d4b3c6f433cb0930f6cbb2b20633409
pavanpwr24 <pavan.pwr24@gmail.com>
2025-06-22 21:59:03 +0530

    modified lambda code to build successfully

--- lambda_cicd.py
+++ lambda_cicd.py
@@ -1,13 +1,9 @@
 import json
-import pandas as pd
 import requests
 
 def lambda_function(event,context):
     print("event",event)
     response=requests.get("https://www.google.com/")
     print(response.text)
-    d1={"col1":[3,5],"col2":[8,9]}
-    df=pd.DataFrame(data=d1)
-    print(df)
 
 
